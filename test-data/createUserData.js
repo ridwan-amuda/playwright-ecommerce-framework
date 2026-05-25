51116c28bb405b2ea360facf5e8eba6d93964667
Ridwan Amuda <ridwanamuda26@outlook.com>
2026-05-25 12:48:35 +0100

    refactor: externalize create user API test data

---
+++ test-data/createUserData.js
@@ -0,0 +1,23 @@
+const { generateRandomEmail } = require('../utils/helpers');
+
+const createUserData = {
+  name: 'Test User',
+  email: generateRandomEmail(),
+  password: 'Password123',
+  title: 'Mr',
+  birth_date: '10',
+  birth_month: 'May',
+  birth_year: '1990',
+  firstname: 'Test',
+  lastname: 'User',
+  company: 'Automation Ltd',
+  address1: '123 Test Street',
+  address2: 'Flat 1',
+  country: 'Canada',
+  zipcode: '12345',
+  state: 'Ontario',
+  city: 'Toronto',
+  mobile_number: '1234567890'
+};
+
+module.exports = { createUserData };
